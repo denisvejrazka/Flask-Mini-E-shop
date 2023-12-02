cb09daa8f77fdb3cab12941778050fecbbe0b173
denisvejrazka <denisvejrazka@gmail.com>
2023-12-02 14:38:16 +0100

    db

--- app.py
+++ app.py
@@ -6,16 +6,22 @@ client = pymongo.MongoClient("mongodb://127.0.0.1:27017")
 shop = client["shop"]
 customers = shop["customers"]
 products = shop["products"]
-x = { "_id": 1, "name": "red-apple", "price": 2.99, "info": "Tasty, green apple", "category": "fruit", "display_name": "Red Apple"}
 
-fruit= products.insert_one(x)
+def insert_docs():
+    myapples = [
+        {'_id': 1, 'name': 'red-apple', 'price': 2.99, 'info': 'Sweet, red apple', 'category': 'fruit', 'display_name': 'Red Apple', 'quantity': 100},
+        {'_id': 2, 'name': 'green-apple', 'price': 2.99, 'info': 'Sour, green apple', 'category': 'fruit', 'display_name': 'Green Apple', 'quantity': 100},
+    ]
+    apples = products.insert_many(myapples)
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    fruit = products.find()
+    return render_template('index.html', fruit=fruit)
 
 @app.route("/fruit")
 def fruit():
-    return render_template("fruit.html", fruit=x)
+    return render_template("fruit.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
