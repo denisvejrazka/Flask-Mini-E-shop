b99561693ba8b1a0bde097ba2be2a4a5dea2adef
denisvejrazka <denisvejrazka@gmail.com>
2023-12-12 22:37:15 +0100

    Update Vegetable + cart

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import pymongo
 
 app = Flask(__name__)
@@ -6,22 +6,63 @@ client = pymongo.MongoClient("mongodb://127.0.0.1:27017")
 shop = client["shop"]
 customers = shop["customers"]
 products = shop["products"]
+#orders = shop["orders"]
 
-def insert_docs():
-    myapples = [
-        {'_id': 1, 'name': 'red-apple', 'price': 2.99, 'info': 'Sweet, red apple', 'category': 'fruit', 'display_name': 'Red Apple', 'quantity': 100},
-        {'_id': 2, 'name': 'green-apple', 'price': 2.99, 'info': 'Sour, green apple', 'category': 'fruit', 'display_name': 'Green Apple', 'quantity': 100},
-    ]
-    apples = products.insert_many(myapples)
+cart = []
+products.update_one({"display_name": "Pear"}, {"$set": {"quantity": 70}})
+quantities = []
+def get_quantites():
+    for product in products.find():
+        quantities.append(product["quantity"])
+    return quantities
+get_quantites()
 
 @app.route("/")
 def index():
-    fruit = products.find()
-    return render_template('index.html', fruit=fruit)
+    return render_template('index.html')
 
 @app.route("/fruit")
 def fruit():
-    return render_template("fruit.html")
+    fruit = products.find({"category": "fruit"})
+    return render_template("fruit.html", fruit=fruit)
+
+@app.route("/add_fruit_to_cart", methods=['POST'])
+def add_fruit_to_cart():
+    selected_fruit_id = request.form.get('product-btn')
+    desired_fruit = products.find_one({"_id": int(selected_fruit_id)})
+    if desired_fruit and desired_fruit['quantity'] > 0:
+        products.update_one({"_id": int(selected_fruit_id)}, {"$inc": {"quantity": -1}})
+        cart.append(desired_fruit)
+    return redirect(url_for('fruit'))
+
+@app.route("/cart")
+def shopping_cart():
+    for product_id in cart:
+        product = products.find({"_id": product_id})
+        
+    return render_template("cart.html", cart=cart)
+
+@app.route("/vegetables")
+def vegetables():
+    vegetables = products.find({"category": "vegetable"})
+    return render_template("vegetables.html", vegetables=vegetables)
+
+@app.route("/add_vege_to_cart", methods=['POST'])
+def add_vege_to_cart():
+    selected_vege_id = request.form.get('product-btn')
+    desired_vege = products.find_one({"_id": int(selected_vege_id)})
+    if desired_vege and desired_vege['quantity'] > 1:
+        products.update_one({"_id": int(selected_vege_id)}, {"$inc": {"quantity": -1}})
+        cart.append(desired_vege)
+    return redirect(url_for('vegetables'))
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
